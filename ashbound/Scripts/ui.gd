54ac52a1a7c7c1be78b7dfeb2c77625fcc0c75ba
Matthew-CS11 <2260213@flourbluffschools.org>
2026-04-17 12:41:06 -0500

    adding spawnpoints in boss room

--- ashbound/Scripts/ui.gd
+++ ashbound/Scripts/ui.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 class_name UI
 
 @onready var ammo_label: Label = $Ammo_HUD/VBoxContainer/ammo_label
+@onready var texture_progress_bar: TextureProgressBar = $Level_progress/TextureProgressBar
 
 
 func update_ammo_label(current, reserve):
