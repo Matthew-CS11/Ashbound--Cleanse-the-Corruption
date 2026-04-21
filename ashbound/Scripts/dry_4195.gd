6828136c4755ee0336a4031a2735ee5149167378
roomno17 <2261021@flourbluffschools.org>
2026-04-21 11:57:01 -0500

    Revert "Reapply "made wall code and added music""
    This reverts commit 9e44c9bb06d6ba2e744779d70017ccce4a4790b0.

--- ashbound/Scripts/dry_4195.gd
+++ ashbound/Scripts/dry_4195.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 var on_fire
-signal burned
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	on_fire = true
@@ -11,8 +10,6 @@ func _process(delta: float) -> void:
 	$Dry8873LOD.transparency+=0.02
 	$Dry8873LOD007.transparency+=0.02
 	$Dry8873LOD008.transparency+=0.02
-	if $Dry8873LOD008.transparency >.99:
-		burned.emit()
 
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
--- ashbound/csg_box_3d_6.gd
+++
@@ -1,5 +0,0 @@
-extends CSGBox3D
-var done = false
-func _process(delta: float) -> void:
-	if done:
-		visible=false
