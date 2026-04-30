47bf3589e63ff547152b706d6a71406785e93653
roomno17 <2261021@flourbluffschools.org>
2026-04-30 12:40:01 -0500

    tutorial changes

--- ashbound/Scripts/tutorial.gd
+++ ashbound/Scripts/tutorial.gd
@@ -1,16 +1,17 @@
 extends Node3D
 @onready var label: Label = $UI/Label
 func _ready() -> void:
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(4).timeout
 	label.text = "the creature in front of you is a mold meister"
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(4).timeout
 	label.text = "whats in your hand is your pistol,
 	 you can shoot the mold meisters with this"
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(4).timeout
 	label.text = "this is your flamethrower. do not use these on the 
 	mold meisters, it will only make them stronger."
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(4).timeout
 	$Enemy.visible=false
 	$mold.visible=true
 	label.text = "use the flamethrower to destroy and burn these viny growths
 	of mold"
+	get_tree().change_scene_to_file("res://Scenes/hospital.tscn")
